e30a211cbb7fefb2eba444fdb22ba0c0e04c3aae
Mariliis Toon <mariliistoon@gmail.com>
2023-12-18 06:18:38 +0200

    render

--- bridgeTable.sql
+++ bridgeTable.sql
@@ -1,14 +1,35 @@
-INSERT INTO ingredientinrecipe(recipeid, ingredientid)
-VALUES (1,1);
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 3);
 
-ISERT INTO ingredientinrecipe(recipeid,ingredientid)
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 4);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
 SELECT a.id, b.id
 FROM recipe a
-JOIN ingredient b
-ON a.recipeName = 'Pumpkin Tartlets' AND b.ingredientname = 'pumkin puree'
+JOIN ingredient b 
+ON a.recipeName = 'Pumpkin Pasties' AND b.ingredientname = 'cloves';
 
-SELECT a.recipeName, b.ingredientName FROM recipe a 
-INNER JOIN ingredientInRecipe c 
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+SELECT a.id, b.id
+FROM recipe a
+JOIN ingredient b 
+ON a.recipeName = 'Pumpkin Pasties' AND b.ingredientname = 'Pastry dough';
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+SELECT a.id, b.id
+FROM recipe a
+JOIN ingredient b 
+ON a.recipeName = 'Pumpkin Pasties' AND b.ingredientname = 'Egg wash (1 egg beaten with a splash of milk)';
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+SELECT a.id, b.id
+FROM recipe a
+JOIN ingredient b 
+ON a.recipeName = 'Pumpkin Tartlets' AND b.ingredientname = 'pumpkin puree';
+
+SELECT a.recipeName, b.ingredientName FROM recipe a
+INNER JOIN IngredientInRecipe c
 ON a.id = c.recipeId
 INNER JOIN ingredient b
 ON b.id = c.ingredientId;
--- db.js
+++ db.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const { Pool } = require('pg');
-const itemsPool = new Pool({
-    user: 'postgres',
-    password: 'XXX', //changed
-    host: 'localhost',
-    port: 5432,
-    database: 'myrecipes'
+
+const itemsPool = new Pool ({
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 
 module.exports = itemsPool; 
